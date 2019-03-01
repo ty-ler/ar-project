9a9442568e97415b725665fa834958d49d591558
Tyler Smith <tyler.smith803@gmail.com>
2019-03-01 16:44:58 -0500

    Added main_menu, login scenes. Improved .gitignore, cleaned up repo

---
+++ game/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public Button LoginButton, RegisterButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoginButton.onClick.AddListener(LoadLoginScene);
+        RegisterButton.onClick.AddListener(LoadRegisterScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadLoginScene()
+    {
+        SceneManager.LoadScene("login");
+    }
+
+    void LoadRegisterScene()
+    {
+        SceneManager.LoadScene("register");
+    }
+}
