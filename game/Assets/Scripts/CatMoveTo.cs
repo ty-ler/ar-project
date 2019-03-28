6e8ca38f39800fed29aa9e22a60200f1f20367c5
Wei Zhang <Wei@Weis-MBP.home>
2019-03-28 00:34:05 -0400

    Added running triggers to the cat, when the user tap the screen the cat is able to run towards the destination

--- game/Assets/Scripts/CatMoveTo.cs
+++ game/Assets/Scripts/CatMoveTo.cs
@@ -17,8 +17,12 @@ public class CatMoveTo : MonoBehaviour
     // Total distance between the markers.
     private float journeyLength;
 
+    private Animator catAnim;
+
+
     void Start() {
         journeyLength = 0;
+        catAnim = GetComponent<Animator>();
     }
 
     // Follows the target position like with a spring
@@ -42,11 +46,17 @@ public class CatMoveTo : MonoBehaviour
                 transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 10f);
             }
         }
+        if (Vector3.Distance(startMarker.position, endMarker) < 0.1) 
+        {
+            catAnim.SetBool("IsRunning", false);
+        }
 
     }
 
     public void StartMove(Vector3 endPos) 
     {
+        //Sets the isrunning state to true, so the cat starts running.
+        catAnim.SetBool("IsRunning", true);
         startMarker = this.transform;
         endMarker = endPos;
         // Keep a note of the time the movement started.
