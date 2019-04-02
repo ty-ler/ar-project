1d87989606b216193254125ed3d7ba0cc549f03a
Tyler Smith <tyler.smith803@gmail.com>
2019-04-02 17:29:27 -0400

    Spawn plane for cat to walk on

--- game/Assets/Scripts/PetController.cs
+++ game/Assets/Scripts/PetController.cs
@@ -8,17 +8,22 @@ public class PetController : MonoBehaviour
 {
     public GameObject pet;
     public GameObject placementIndicator;
+    public GameObject petPlane;
 
     private ARSessionOrigin arOrigin;
     private Pose placementPose;
     private bool validPlacementPose = false;
     private int maxCatAllowed = 1;
     private int currentNumberOfCats = 0;
+    private bool placed = false;
+    Vector3 petPlanePos;
 
     // Start is called before the first frame update
     void Start()
     {
         arOrigin = FindObjectOfType<ARSessionOrigin>();
+        pet.SetActive(false);
+        petPlane.SetActive(false);
     }
 
     // Update is called once per frame
@@ -44,42 +49,79 @@ public class PetController : MonoBehaviour
 
     void UpdatePlacementIndicator()
     {
-        if(validPlacementPose)
+        if(!placed)
         {
-            placementIndicator.SetActive(true);
-            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
-
-            pet.SetActive(true);
+            if (validPlacementPose)
+            {
+                placementIndicator.SetActive(true);
+                placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+            }
+            else
+            {
+                placementIndicator.SetActive(false);
+            }
         } else
         {
-            placementIndicator.SetActive(false);
+            if(validPlacementPose)
+            {
+                placementIndicator.SetActive(true);
+                placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+            } else
+            {
+                placementIndicator.SetActive(false);
+            }
         }
     }
 
     void UpdatePlacementPose()
     {
-        var screencenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        Vector3 screencenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var cameraForward = Camera.current.transform.forward;
+        var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
 
-        var hits = new List<ARRaycastHit>();
-        
-        arOrigin.Raycast(screencenter, hits, TrackableType.Planes);
-
-        validPlacementPose = hits.Count > 0;
-
-        Debug.Log(validPlacementPose);
-
-        if(validPlacementPose)
+        if (!placed)
         {
-            placementPose = hits[0].pose;
+            List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
-            var cameraForward = Camera.current.transform.forward;
-            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
-            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+            arOrigin.Raycast(screencenter, hits, TrackableType.Planes);
+
+            validPlacementPose = hits.Count > 0;
+
+            if (validPlacementPose)
+            {
+                placementPose = hits[0].pose;
+                placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+            }
+        }
+        else
+        {
+            RaycastHit hit;
+            Debug.Log("Raycasting!");
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit))
+            {
+                Debug.Log("Hit!");
+                validPlacementPose = true;
+                placementPose.position = hit.point;
+                placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+            }
+            else
+            {
+                validPlacementPose = false;
+            }
         }
     }
 
     void PlacePet()
     {
-        pet = Instantiate(pet, placementPose.position, placementPose.rotation);
+        placed = true;
+
+        Vector3 placePosePos = placementPose.position;
+
+        petPlanePos = new Vector3(placePosePos.x, placePosePos.y, placePosePos.z);
+
+        petPlane = Instantiate(petPlane, placePosePos, placementPose.rotation);
+        pet = Instantiate(pet, placePosePos, placementPose.rotation);
+        pet.SetActive(true);
+        petPlane.SetActive(true);
     }
 }
