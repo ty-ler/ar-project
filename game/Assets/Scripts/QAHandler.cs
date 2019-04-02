77ad9517a762b3bfda8b186316f89d236c8cab72
ToneCat <tcatalfamo17@aol.com>
2019-04-02 12:14:37 -0400

    added food and in game GUI to view question/answers

---
+++ game/Assets/Scripts/QAHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QAHandler : MonoBehaviour
+{
+    public GameObject Panel;
+    public Button button;
+    void Start()
+    {
+    Panel.gameObject.SetActive(false);
+    button.onClick.AddListener(OnClick);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnClick()
+    {
+    Panel.SetActive(!Panel.activeSelf);
+    }
+}
