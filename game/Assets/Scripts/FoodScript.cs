cb5dba3a31b0052881588b4d3f141185cea54cc2
Tyler Smith <tyler.smith803@gmail.com>
2019-04-04 02:29:29 -0400

    Added floating text values for food, check for correct answer on collide

--- game/Assets/Scripts/FoodScript.cs
+++ game/Assets/Scripts/FoodScript.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class FoodScript : MonoBehaviour
 {
     public float value { get; set; }
+    public TextMeshPro foodText;
 
-    //void OnTriggerEnter(Collider other)
-    //{
-    //    GameObject collidedObj = other.gameObject;
-    //    string objectTag = collidedObj.tag;
-    //    if (objectTag == "Food")
-    //    {
-    //        Debug.Log("FOOD!!!!!!!!!!!!!!!!!!!!!");
-    //    }
-    //}
+    void Update()
+    {
+        foodText.transform.LookAt(Camera.main.transform);
+        foodText.transform.Rotate(Vector3.up - new Vector3(0, 180, 0));
+    }
+
+    public void setValue(float val)
+    {
+        value = val;
+        foodText.SetText(val.ToString());
+    }
 }
