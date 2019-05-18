60cb5cc9d1a56198195455781a9e19c748979be6
ylerxyz <tyler.smith803@gmail.com>
2019-05-18 14:33:21 -0400

    fix class questions

--- teacher-app/src/components/Class/ClassQuestions/ClassQuestions.js
+++ teacher-app/src/components/Class/ClassQuestions/ClassQuestions.js
@@ -105,7 +105,7 @@ export default class ClassQuestions extends Component {
         </Controls>
 
         <ToolkitProvider
-          keyField="id"
+          keyField="order"
           data={this.state.questions}
           columns={questionColumns}
           search
